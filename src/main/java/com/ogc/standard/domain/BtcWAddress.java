7ae9df203d62827aaab41a0ea16209b5f375f72a
pyez1158 <pyez1158@163.com>
2018-09-10 19:27:50 +0800

    btc地址新增

---
+++ src/main/java/com/ogc/standard/domain/BtcWAddress.java
@@ -0,0 +1,138 @@
+package com.ogc.standard.domain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import com.ogc.standard.dao.base.ABaseDO;
+
+/**
+ * btc平台归集地址
+ * @author: xieyj 
+ * @since: 2018年2月7日 下午2:08:49 
+ * @history:
+ */
+public class BtcWAddress extends ABaseDO {
+
+    private static final long serialVersionUID = 1L;
+
+    // ID主键
+    private Long id;
+
+    // btc地址
+    private String address;
+
+    // 状态
+    private String status;
+
+    // 创建时间
+    private Date createDatetime;
+
+    /* DB properties 结束 */
+
+    // 状态列表
+    private List<String> statusList;
+
+    // 类型列表
+    private List<String> typeList;
+
+    // 地址模糊查询
+    private String addressForQuery;
+
+    private Long useCount;
+
+    private BigDecimal useAmount;
+
+    private String useAmountString;
+
+    private String balanceString;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getCreateDatetime() {
+        return createDatetime;
+    }
+
+    public void setCreateDatetime(Date createDatetime) {
+        this.createDatetime = createDatetime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<String> getStatusList() {
+        return statusList;
+    }
+
+    public void setStatusList(List<String> statusList) {
+        this.statusList = statusList;
+    }
+
+    public List<String> getTypeList() {
+        return typeList;
+    }
+
+    public void setTypeList(List<String> typeList) {
+        this.typeList = typeList;
+    }
+
+    public String getAddressForQuery() {
+        return addressForQuery;
+    }
+
+    public void setAddressForQuery(String addressForQuery) {
+        this.addressForQuery = addressForQuery;
+    }
+
+    public Long getUseCount() {
+        return useCount;
+    }
+
+    public void setUseCount(Long useCount) {
+        this.useCount = useCount;
+    }
+
+    public BigDecimal getUseAmount() {
+        return useAmount;
+    }
+
+    public void setUseAmount(BigDecimal useAmount) {
+        this.useAmount = useAmount;
+        this.useAmountString = useAmount.toString();
+    }
+
+    public String getUseAmountString() {
+        return useAmountString;
+    }
+
+    public void setUseAmountString(String useAmountString) {
+        this.useAmountString = useAmountString;
+    }
+
+    public String getBalanceString() {
+        return balanceString;
+    }
+
+    public void setBalanceString(String balanceString) {
+        this.balanceString = balanceString;
+    }
+}
