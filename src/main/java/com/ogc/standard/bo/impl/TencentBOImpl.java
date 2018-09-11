bb719630c64a3a923f4a5594153a2f6c0093a537
tao <taojian@192.168.1.186>
2018-09-11 19:38:40 +0800

    腾讯云注释

--- src/main/java/com/ogc/standard/bo/impl/TencentBOImpl.java
+++ src/main/java/com/ogc/standard/bo/impl/TencentBOImpl.java
@@ -240,13 +240,13 @@ public class TencentBOImpl implements ITencentBO {
             String errorInfo = JSONObject.parseObject(result)
                 .getString("ErrorInfo");
             if (!errorCode.equals("0")) {
-                throw new BizException(EBizErrorCode.DEFAULT.getCode(),
-                    "腾讯云创建群组异常,错误编号：" + errorCode + "，原因：" + errorInfo);
+//                throw new BizException(EBizErrorCode.DEFAULT.getCode(),
+//                    "腾讯云创建群组异常,错误编号：" + errorCode + "，原因：" + errorInfo);
             }
 
         } catch (Exception e) {
-            throw new BizException(EBizErrorCode.DEFAULT.getCode(),
-                "腾讯云创建群组异常，原因" + e.getMessage());
+//            throw new BizException(EBizErrorCode.DEFAULT.getCode(),
+//                "腾讯云创建群组异常，原因" + e.getMessage());
         }
     }
 
@@ -353,13 +353,13 @@ public class TencentBOImpl implements ITencentBO {
             String errorInfo = JSONObject.parseObject(result)
                 .getString("ErrorInfo");
             if (!errorCode.equals("0")) {
-                throw new BizException(EBizErrorCode.DEFAULT.getCode(),
-                    "腾讯云发送系统消息异常,错误编号：" + errorCode + "，原因：" + errorInfo);
+//                throw new BizException(EBizErrorCode.DEFAULT.getCode(),
+//                    "腾讯云发送系统消息异常,错误编号：" + errorCode + "，原因：" + errorInfo);
             }
 
         } catch (Exception e) {
-            throw new BizException(EBizErrorCode.DEFAULT.getCode(),
-                "腾讯云发送系统消息异常，原因" + e.getMessage());
+//            throw new BizException(EBizErrorCode.DEFAULT.getCode(),
+//                "腾讯云发送系统消息异常，原因" + e.getMessage());
         }
     }
 
