78187ff5b1935f63e4a81f858500f15d405cb114
lei <lei@192.168.1.184>
2018-09-19 16:41:36 +0800

    bug fix

--- src/main/java/com/ogc/standard/common/CoinUtil.java
+++ src/main/java/com/ogc/standard/common/CoinUtil.java
@@ -1,11 +1,3 @@
-/**
- * @Title CoinUtil.java 
- * @Package com.ogc.standard.wallet.common 
- * @Description 
- * @author haiqingzheng  
- * @date 2018年3月13日 上午11:36:14 
- * @version V1.0   
- */
 package com.ogc.standard.common;
 
 import java.math.BigDecimal;
