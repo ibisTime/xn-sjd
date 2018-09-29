c2b2ec5707de3227e1441389377f239cdb0507ad
silver <silver@kekaideair>
2018-09-29 20:58:54 +0800

    修改账户相关

--- src/main/java/com/ogc/standard/bo/IAccountBO.java
+++ src/main/java/com/ogc/standard/bo/IAccountBO.java
@@ -20,7 +20,7 @@ public interface IAccountBO extends IPaginableBO<Account> {
 
     // 分配账户
     public String distributeAccount(String userId, EAccountType accountType,
-            String address);
+            String currency);
 
     // 变更账户余额：流水落地
     public Account changeAmount(Account dbAccount, BigDecimal transAmount,
